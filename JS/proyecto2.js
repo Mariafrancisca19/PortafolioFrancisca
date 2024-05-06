eae46ae689e2ed6a61b2a4d0e33258210213444e
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-05-06 13:54:20 -0600

    Estructura del portafolio

---
+++ JS/proyecto2.js
@@ -0,0 +1,16 @@
+let inputNombre = document.getElementById("gmail")
+let inputContrasena= document.getElementById("pass")
+let iniciar=document.getElementById("iniciar")
+
+let nombre;
+let clave;
+
+function recuperarDatos(){
+    nombre = inputNombre.value
+    clave = inputContrasena.value
+
+}
+
+iniciar.addEventListener("click",function() {
+    recuperarDatos()
+})
