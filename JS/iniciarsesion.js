f279b49d29c6bebb9afbed89aae71b6101bb39e5
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-05-09 16:01:47 -0600

    Registrarse

--- JS/iniciarsesion.js
+++ JS/iniciarsesion.js
@@ -1,3 +1,4 @@
+
 let inputCorreo = document.getElementById("gmail")
 let inputContrasena= document.getElementById("pass")
 let iniciar=document.getElementById("iniciar")
@@ -11,20 +12,20 @@ let btnCheckIn = document.getElementById("registrarse")
         
 //}
 
-iniciar.addEventListener("click", (e) => {
+iniciar.addEventListener("click", () => {
    const Users = JSON.parse(localStorage.getItem("nombres"))
 
    console.log(Users[0].correo)
    console.log(inputCorreo.value)
 
-   if (inputCorreo.value == Users[0].correo) {
+   if (inputCorreo.value == Users[0].correo && inputContrasena.value==Users[0].contrase) {
     window.location.href="/Portafolio/portafoliopersonal.html"
    }
 
    if (validacionDeUsuario) {
     alert("Datos Correctos")
    
-    window.location.href="proyecto2.html"
+    window.location.href="iniciarsesion.html"
    } else {
     alert("Datos inccorectos")
    }                   
